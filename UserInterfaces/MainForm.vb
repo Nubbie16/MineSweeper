be74db5c6234a4c0b626544ca370d861ed727c05
Cody Eckard <eckard.cody@gmail.com>
2026-05-25 11:27:44 -0400

    Folder Structure and initial file comment

---
+++ UserInterfaces/MainForm.vb
@@ -0,0 +1,12 @@
+﻿'Project Name: MineSweeper
+'Purpose: MineSweeper game that stores high scores(fastest completion)
+'File Name: MainForm.vb
+'Description: This is the main form for the MineSweeper game. It will display
+'             the start button to begin a game and high scores based on completion time.
+'Programmer: Cody Eckard
+'Creation Date: May 25, 2026
+'GitHub Repository: https://github.com/Nubbie16/MineSweeper
+
+Public Class MainForm
+
+End Class
